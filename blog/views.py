a191ad86cdee94c5fe581d9802f24d1fc190d177
4dmin <marishka@outlook.ph>
2019-03-15 12:26:09 +0700

    blog and job up and running on cctv site project

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
-# Create your views here.
+
+def allblogs(request):
+    allblogs = Blog.objects
+    return render(request, 'blog/home.html', {'allblogs':allblogs})
+
+def blogdetails(request, blog_id):
+    details = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/blogdetails.html', {'details':details})
+
+    
